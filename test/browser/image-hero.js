db96e90a762178b7b7c67bbb654a44b97671b4e0
Blake Kostner <git@btkostner.io>
2020-03-06 10:36:38 -0700

    add a super basic browser test

---
+++ test/browser/image-hero.js
@@ -0,0 +1,11 @@
+import { Selector } from 'testcafe'
+
+fixture `Image Hero`
+  .page `http://localhost:9531/iframe.html?id=components-sysassetimagehero--example&viewMode=story`
+
+const container = Selector('#root')
+const hero = Selector('picture')
+
+test('spans the full width', async (t) => {
+  await t.expect(hero.clientWidth).eql(await container.clientWidth)
+})
